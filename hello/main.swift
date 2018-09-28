a7be751a469088a29ec70a21e8d67b84628b5aca
王蕾 <867550109@qq.com>
2018-09-28 21:17:15 +0800

    Initial Commit

---
+++ hello/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hello
+//
+//  Created by s20171106184 on 2018/9/28.
+//  Copyright © 2018年 s20171106184. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
